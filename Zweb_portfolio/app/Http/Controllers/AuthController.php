79e15cb79e1fceffac4328f89cdb9adfec873bfe
BroCooLyZ <siranthoinezingalaoa@gmail.com>
2025-11-21 23:42:39 +0800

    still showing landing page

---
+++ Zweb_portfolio/app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    function showRegister(){return view('authentication.register');}
+    function register(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|string|max:255',
+            'email'=>'required|string|email|max:100|unique:users',
+            'password'=>'required|min:8|confirmed|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/'
+        ]);
+
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+        ]);
+
+        return redirect()->route('login.form')->with('success', 'Registration successful. Please log in.');
+    }
+
+    function showLogin(){return view('authentication.login');}
+    function login(Request $request);
+    {
+        $credentials = $request->validate([
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid email/password! The provided credentials do not match our records.',
+        ]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
